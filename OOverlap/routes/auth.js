39138c95d6f7dd4fba224dc66955ca869da93853
Thai Nguyen <thainguyen@umass.edu>
2014-11-18 15:32:19 -0500

    add auth

---
+++ OOverlap/routes/auth.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var querystring = require('querystring');
+
+router.post('/', function(req, res) {
+	var query = querystring.parse(url.parse(req.url).query);
+	var username = query.username;
+	var location = query.password;
+  res.render('index', { title: 'OOverlap' });
+});
+
+module.exports = route

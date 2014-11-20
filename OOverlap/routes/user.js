d64f4f9f61f9a595f17b234daf3555517955e2b6
Thai Nguyen <thainguyen@umass.edu>
2014-11-20 15:00:32 -0500

    update redirect logic

--- OOverlap/routes/user.js
+++ OOverlap/routes/user.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
-});
-
 router.get('/profile', function(req, res) {
-  res.render('profile', {user: req.user});
+  if (req.user){
+  	res.render('profile', {user: req.user});
+  } else {
+  	res.redirect('/');
+  }
 });
 
 module.exports = router;

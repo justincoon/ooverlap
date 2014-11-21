64879811911a6b62651815c336cd361db2d17e3e
justincoon <justin.tyler.coon@gmail.com>
2014-11-21 13:58:50 -0500

    fixed it so we only pull future events from google calendar

--- OOverlap/routes/user.js
+++ OOverlap/routes/user.js
@@ -11,17 +11,15 @@ router.get('/profile', function(req, res) {
 });
 
 router.get('/calendar', function(req, res) {
-  if (req.user) {
-    var google_calendar = new gcal.GoogleCalendar(req.user.tokens[0].accessToken);
-    console.log(req.user.tokens[0].accessToken);
-    google_calendar.calendarList.list(function(err, calendarList) {
-	google_calendar.events.list(req.user.email, function(err, calendarList) {
-		res.send(calendarList);
-	});
-    });
-  } else {
-    res.redirect('/');
-  }
+	if (req.user) {
+    		var google_calendar = new gcal.GoogleCalendar(req.user.tokens[0].accessToken);
+		google_calendar.events.list(req.user.email, {'timeMin': (new Date()).toISOString()}, function(err, calendarList) {
+			console.log(calendarList.items);
+			res.send(calendarList);
+		});
+	} else {
+		res.redirect('/');
+	}
 });
 
 router.get('/logout', function(req, res) {

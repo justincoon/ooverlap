59cc3ea09a7240279cb3f6bc5af55fd79dd9ae1a
Thai Nguyen <thainguyen@umass.edu>
2014-11-28 22:31:13 -0500

    fix UX when add a new friend

--- OOverlap/public/javascripts/profile.js
+++ OOverlap/public/javascripts/profile.js
@@ -44,54 +44,53 @@ function jQuery_FindFriend(email, callback) {
 	});
 }
 
-function jQuery_BindFindFriend() {
-	// For Getting Friend Info From Server:
-	$('input#find-new-friend-input').bind('keypress',
-		function(event) {
-			if (event.keyCode === 13) {
-				// Get the friend email to lookup:
-				var email = $('input#find-new-friend-input').val();
-				if (email.indexOf('@') < 0 && email.indexOf('.') < 0) {
-					$('#friend-output').html('Please provide a valid email address');
-					return false;
-				}
-				$('#friend-output').html('Finding..');
-
-				jQuery_FindFriend(email, function(data) {
-					if (data.error) {
-						$('#friend-output').html('Cannot find user with email ' + email);
-					} else if (data.self){
-						$('#friend-output').html('Sorry you cannot add yourself as a friend');
-					} else if (data.friend_exist) {
-						$('#friend-output').html('User with email ' + email + ' already in your friend list');
-					} else if (data.request_sent_exist) {
-						$('#friend-output').html('You already sent a request to user with email ' + email);
-					} else if (data.request_received_exist) {
-						$('#friend-output').html('User with email ' + email + ' already sent your a request, please check your pending requests');
-					} else {
-						$('#friend-output').html(data.friend.name + " " + data.friend.email);
-					}
-				});
-				// Reset input field:
-				$('input').val('');
-				// return false;
-			}
-		});
-}
-
 function jQuery_BindAddFriend(){
 	$('#add_friend').bind('click',
 		function(event){
-			$.ajax({
-				type: 'GET',
-				url: '/user/request/friend',
-			}).done(function(msg) {
-				if (msg.status){
-					generate('Succesfully sending friend request','success');
-				} else {
-					generate('Fail to send friend request, please try again','error');
+			var email = $('input#find-new-friend-input').val();
+			if (email.indexOf('@') < 0 && email.indexOf('.') < 0) {
+					generate('Please provide a valid email address','error');
+					$('#friend-output').html('');
+					return false;
 				}
-			});
+			$('#friend-output').html('Finding..');
+			jQuery_FindFriend(email, function(data) {
+					if (data.error) {
+						generate('Cannot find user with email ' + email,'error');
+						$('#friend-output').html('');
+						return false;
+					} else if (data.self){
+						generate('Sorry you cannot add yourself as a friend', 'error');
+						$('#friend-output').html('');
+						return false;
+					} else if (data.friend_exist) {
+						generate('User with email ' + email + ' already in your friend list', 'error');
+						$('#friend-output').html('');
+						return false;
+					} else if (data.request_sent_exist) {
+						generate('You already sent a request to user with email ' + email, 'error');
+						$('#friend-output').html('');
+						return false;
+					} else if (data.request_received_exist) {
+						generate('User with email ' + email + ' already sent your a request, please check your pending requests')
+						$('#friend-output').html('');
+						return false;
+					} else {
+						$('#friend-output').html(data.friend.name + " " + data.friend.email);
+						$.ajax({
+							type: 'GET',
+							url: '/user/request/friend',
+						}).done(function(msg) {
+							console.log("!!!");
+							$('#friendEventModal_1').modal('hide')
+							if (msg.status){
+								generate('Succesfully sending friend request','success');
+							} else {
+								generate('Fail to send friend request, please try again','error');
+							}
+						});
+					}
+				});
 		});
 }
 
@@ -198,7 +197,6 @@ $(document).ready(function() {
 	});
 	//Binding functions
 	jQuery_BindGetFriend();
-	jQuery_BindFindFriend();
 	jQuery_BindAddFriend();
 	$('[data-toggle=tooltip]').tooltip();
 });

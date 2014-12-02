9366e9b9c39bd1ee76cd724c368e835bfc21e732
justincoon <justin.tyler.coon@gmail.com>
2014-12-02 16:34:10 -0500

    added submit changes onclick jquery binding

---
+++ OOverlap/public/javascripts/settings.js
@@ -0,0 +1,54 @@
+function jQuery_BindSubmitRequest() {
+	$('#submit_changes').bind('click',
+		function(event) {
+			var settings = {
+				profilePicPrivacy: $('#profilePicPrivacy').val(),
+				emailPrivacy: $('#profilePicPrivacy').val(),
+				profPic: $('#newProfilePicture_text').val(),
+				name: $('#displayedName').val(),
+				email: $('#email').val(),
+				currentPassword: $('#password_input').val(),
+				newPassword: $('#new_password').val(),
+				newPasswordConfirm: $('#new_password_cf').val()
+			};
+			if (settings.newPassword) {
+				if(!jQuery_CheckPassword(settings.currentPassword)) {
+					generate('Incorrect password, please try again', 'error');
+					return false;
+				}
+				if(settings.newPassword !== settings.newPasswordConfirm) {
+					generate('Passwords do not match, please try again', 'error');
+					return false;
+				}
+			}
+			if (settings.email.indexOf('@') < 0 && settings.email.indexOf('.') < 0) {
+				generate('Please provide a valid email address', 'error');
+				return false;
+			}
+			jQuery_ChangeSettings(settings);
+		});
+}
+
+function jQuery_CheckPassword(password) {
+	
+}
+
+function jQuery_ChangeSettings(settings) {
+	
+}
+
+function generate(text, type) {
+ 	var n = noty({
+ 		text: text,
+ 		type: type,
+ 		dismissQueue: true,
+ 		layout: 'topCenter',
+ 		theme: 'defaultTheme',
+ 		timeout: 5000,
+ 		maxVisible: 10
+ 	});
+}
+
+$(document).ready(function() {
+	jQuery_BindSubmitRequest();
+});

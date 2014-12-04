cff6652c6549522e6e6eaff43aa96bc2aff2e80c
Thai Nguyen <thainguyen@umass.edu>
2014-12-04 00:43:22 -0500

    change to use bootstrap modal instead of leanModal

---
+++ OOverlap/public/javascripts/auth.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$(function(){
+        // Calling Login Form
+        $("#login_form").click(function(){
+            $(".social_login").hide();
+            $(".user_login").show();
+            return false;
+        });
+
+        // Calling Register Form
+        $("#register_form").click(function(){
+            $(".social_login").hide();
+            $(".user_register").show();
+            $(".header_title").text('Register');
+            return false;
+        });
+
+        // Going back to Social Forms
+        $(".back_btn").click(function(){
+            $(".user_login").hide();
+            $(".user_register").hide();
+            $(".social_login").show();
+            $(".header_title").text('Login');
+            return false;
+        });
+    });
+});

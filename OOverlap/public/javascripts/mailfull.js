6f45c1768c1ccb4d79060d7ab83e4f716fb144f5
AWiredMind <kdrohan@umass.edu>
2014-12-08 20:38:32 -0500

    Update mailfull.js

--- OOverlap/public/javascripts/mailfull.js
+++ OOverlap/public/javascripts/mailfull.js
@@ -1,34 +1,25 @@
-//We're using the express framework and the mailgun-js wrapper
+
 var express = require('express');
 var Mailgun = require('mailgun-js');
-//init express
 var app = express();
 
 
-//Your api key, from Mailgun’s Control Panel
 var api_key = 'key-a35505ce372dbcf1d1497174eced1ed9';
 
-//Your domain, from the Mailgun Control Panel
 var domain = 'OOverlap.com';
 
-//Your sending email address
 var from_who = 'mg.overlap.com';
 
-//Tell express to fetch files from the /js directory
 app.use(express.static(__dirname + '/js'));
 
 app.set('view engine', 'jade')
 
-//Do something when you're landing on the first page
 app.get('/', function(req, res) {
-    //render the index.jade file - input forms for humans
     res.render('index', function(err, html) {
         if (err) {
-            // log any error to the console for debug
             console.log(err); 
         }
         else { 
-            //no error, so send the html to the browser
             res.send(html)
         };
     });
@@ -39,25 +30,20 @@ app.get('/submit/:mail', function(req,res) {
     var mailgun = new Mailgun({apiKey: api_key, domain: domain});
 
     var data = {
-    //Specify email data
       from: from_who,
-    //The email to contact
       to: req.params.mail,
-    //Subject and text data  
       subject: 'Hello from OOverlap',
       html: 'Welcome to OOverlap'
     }
 
     mailgun.messages().send(data, function (err, body) {
-        //If there is an error, render the error page
         if (err) {
             res.render('error', { error : err});
             console.log("got an error: ", err);
         }
-        //Else we can greet    and leave
+
         else {
-            //Here "submitted.jade" is the view file for this landing page 
-            //We pass the variable "email" from the url parameter in an object rendered by Jade
+    
             res.render('submitted', { email : req.params.mail });
             console.log(body);
         }
@@ -69,25 +55,18 @@ app.get('/forgot/:mail', function(req,res) {
     var mailgun = new Mailgun({apiKey: api_key, domain: domain});
 
     var data = {
-    //Specify email data
       from: from_who,
-    //The email to contact
       to: req.params.mail,
-    //Subject and text data  
       subject: 'Forgotten Email',
       html: ''
     }
 
     mailgun.messages().send(data, function (err, body) {
-        //If there is an error, render the error page
         if (err) {
             res.render('error', { error : err});
             console.log("got an error: ", err);
         }
-        //Else we can greet    and leave
         else {
-            //Here "submitted.jade" is the view file for this landing page 
-            //We pass the variable "email" from the url parameter in an object rendered by Jade
             res.render('submitted', { email : req.params.mail });
             console.log(body);
         }
@@ -96,11 +75,9 @@ app.get('/forgot/:mail', function(req,res) {
 });
 
 app.get('/Welcome/:mail', function(req,res){
-    //Which file to send? I made an empty invoice.txt file in the root directory
-    //We required the path module here..to find the full path to attach the file!
+    
     var path = require("path");
     var fp = path.join(__dirname, 'welcome.txt');
-    //Settings
     var mailgun = new Mailgun({apiKey: api_key, domain: domain});
 
     var data = {

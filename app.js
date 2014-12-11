795d4d9271a34d56d6799105402a9204204e9bf7
justincoon <justin.tyler.coon@gmail.com>
2014-12-11 14:24:57 -0500

    made it so you can launch using 'node app.js

--- app.js
+++ app.js
@@ -104,5 +104,11 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Listening on port' + app.get('port'));
+});
+
 
 module.exports = app;
